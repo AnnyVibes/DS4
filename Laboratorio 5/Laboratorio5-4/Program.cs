77126ef66ab529526538910b273e50225b737fce
Anllelina Varcasia <156153390+AnnyVibes@users.noreply.github.com>
2024-09-11 20:47:52 -0500

    Laboratorio 5

---
+++ Laboratorio 5/Laboratorio5-4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Laboratorio5
+{
+    // Problema 5-4
+    internal class Problema5_4
+    {
+        private List<int> calificaciones = new List<int> { 85, 90, 78, 92, 88 };
+        public void CalcularPromedio()
+        {
+            int suma = 0;
+            foreach (int calificacion in calificaciones)
+            {
+                suma += calificacion;
+            }
+            double promedio = suma / (double)calificaciones.Count;
+            Console.WriteLine($"El promedio de las calificaciones es: {promedio}");
+        }
+
+        static void Main(string[] args)
+        {
+            Problema5_4 problema = new Problema5_4();
+            problema.CalcularPromedio();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio 5/Laboratorio5-6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Laboratorio5
+{
+    // Problema 5-6
+    internal class Problema5_6
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+            {
+                {"Francia", "París" },
+                {"España", "Madrid" },
+                {"Italia", "Roma" }
+            };
+
+            foreach (KeyValuePair<string, string> par in paisesYCapitales)
+            {
+                Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+            }
+        }
+    }
+}
