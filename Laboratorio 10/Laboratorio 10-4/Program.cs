2ac8ae6ec55d4ac36a73eb0c5cc6b89e8ff53ec2
Anllelina Varcasia <156153390+AnnyVibes@users.noreply.github.com>
2024-09-29 11:57:05 -0500

    Laboratorio 10

---
+++ Laboratorio 10/Laboratorio 10-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+Module Laboratorio104
+    Sub Main()
+        Try
+            Console.WriteLine("Ingrese un número:")
+            Dim numero As Integer = Convert.ToInt32(Console.ReadLine())
+            Console.WriteLine("El número ingresado es: " & numero)
+        Catch ex As Exception
+            Console.WriteLine("Ocurrió un error: " & ex.Message)
+        Finally
+            Console.WriteLine("Fin del programa.")
+        End Try
+            Console.ReadLine() 
+    End Sub
+End Module
